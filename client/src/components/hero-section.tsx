374b9b57d72f9e1852c22ec962acb9823e9ee2e5
MingsChineseCuisine <mingschinesecuisine123@gmail.com>
2025-09-01 00:21:18 +0530

    Update hero-section.tsx

--- client/src/components/hero-section.tsx
+++ client/src/components/hero-section.tsx
@@ -25,8 +25,8 @@ export default function HeroSection() {
 
   // Video URL with quality parameters for mobile
   const getVideoUrl = () => {
-    const baseUrl = "https://www.youtube.com/embed/PMrmxuJRPAM?si=6HBnCyA2OlO1isbx";
-    const commonParams = "autoplay=1&mute=1&loop=1&playlist=dsm4b__I1Nc&controls=0&showinfo=0&rel=0&iv_load_policy=3&modestbranding=1&disablekb=1&fs=0&cc_load_policy=0&playsinline=1&autohide=1";
+    const baseUrl = "https://www.youtube.com/embed/PMrmxuJRPAM";
+    const commonParams = "autoplay=1&mute=1&loop=1&playlist=PMrmxuJRPAM&controls=0&showinfo=0&rel=0&iv_load_policy=3&modestbranding=1&disablekb=1&fs=0&cc_load_policy=0&playsinline=1&autohide=1";
     
     if (isMobile) {
       // Force higher quality for mobile devices
